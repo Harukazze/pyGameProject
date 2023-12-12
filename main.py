54be4da7b912acaba61aa741e96b5650d40fe536
Harukazze <temapr2006@gmail.com>
2023-12-12 20:02:38 +0300

    Что-то похожее на главное меню

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
 import pygame
 
 
-def draw(screen):
-    screen.fill((0, 0, 0))
-    print('123')
-    font = pygame.font.Font(None, 50)
-    text = font.render("gta 13 нахуй!", True, (100, 255, 100))
-    text_x = width // 2 - text.get_width() // 2
-    text_y = height // 2 - text.get_height() // 2
-    text_w = text.get_width()
-    text_h = text.get_height()
-    screen.blit(text, (text_x, text_y))
-    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
-                                           text_w + 20, text_h + 20), 1)
+class MainMenu:
+    def __init__(self):
+        self.buttons = []
+        self.callback = []
+        self.selected_option = 0
+
+    def append_button(self, button, callback):
+        self.buttons.append(font.render(button, True, (255, 255, 255)))
+        self.callback.append(callback)
+
+    def switch(self, direction):
+        if 0 <= self.selected_option + direction <= len(self.buttons) - 1:
+            self.selected_option += direction
+        else:
+            pass
+
+    def select(self):
+        print('123')
+        self.callback[self.selected_option]()
+
+
+    def draw(self, surface, posX, posY, padding):
+        for i, option in enumerate(self.buttons):
+            option_rect = option.get_rect()
+            option_rect.topleft = (posX, posY + i * padding)
+            if i == self.selected_option:
+                pygame.draw.rect(surface, (20, 20, 20), option_rect)
+            surface.blit(option, option_rect)
 
 
 if __name__ == '__main__':
-    # инициализация Pygame:
+
     pygame.init()
-    # размеры окна:
-    size = width, height = 800, 600
-    # screen — холст, на котором нужно рисовать:
+    size = width, height = 900, 900
     screen = pygame.display.set_mode(size)
-    # формирование кадра:
-    # команды рисования на холсте
-    # ...
-    # ...
-    # смена (отрисовка) кадра:
-    draw(screen)
-    pygame.display.flip()
-    # ожидание закрытия окна:
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    # завершение работы:
-    pygame.quit()
+    font = pygame.font.Font(None, 50)
+
+    menu = MainMenu()
+    menu.append_button('New Game', lambda: print('123321'))
+    menu.append_button('Quit Game', pygame.quit)
+
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    menu.switch(1)
+                elif event.key == pygame.K_s:
+                    menu.switch(-1)
+                elif event.key == pygame.K_SPACE:
+                    menu.select()
+
+        menu.draw(screen, 100, 100, 75)
+        pygame.display.flip()
+pygame.quit()
